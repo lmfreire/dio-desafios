ab0159624f7034dbf93e19e93e599691cc636e16
lmfreire <l.matheus.freire10@gmil.com>
2022-12-12 08:17:30 -0300

    feat: desafio: hospedagem hotel

---
+++ trilha-net-desafio-hospedagem/Models/Reserva.cs
@@ -0,0 +1,51 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Suite não suporta a quantidade de hospedes");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor * 0.10M);
+            }
+
+            return valor;
+        }
+    }
+}
